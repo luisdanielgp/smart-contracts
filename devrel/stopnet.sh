620227305d2161c0dfe33b53a3c0c9ee75e98eb6
Luis Guzman <luisdaniel.guzmanp@gmail.com>
2022-02-28 21:35:36 -0600

    startnet and stopnet scripts execute sandbox

--- devrel/stopnet.sh
+++ devrel/stopnet.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
-../goal network stop -r test
-../goal network delete -r test
+echo "### Tearing down sandbox environment"
+~/Projects/algorand/sandbox/sandbox down # Set local path for sandbox
+echo "Deleting containers"
+docker-compose -f ~/Projects/algorand/sandbox/docker-compose.yml rm --force algod indexer indexer-db
